f5b4e10e291c526c19ccf7ee33212a4ffa68df73
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-05-03 16:53:38 +0300

    none

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-// import { Routes, Route } from "react-router-dom";
-// import DashboardRoutes from "./Pages/Dashboard/DashboardRoutes/DashboardRoutes";
-// import Login from "./Pages/Dashboard/Login/Login";
-// import { UserContext } from "./userContext";
-// import { useState } from "react";
-import TeamCard from "./Components/TeamSection/TeamCard.js";
-import TeamSection from "./Components/TeamSection/TeamSection.js";
-
+import { Routes, Route } from "react-router-dom";
+import DashboardRoutes from "./Pages/Dashboard/DashboardRoutes/DashboardRoutes";
+import Login from "./Pages/Dashboard/Login/Login";
+import { UserContext } from "./userContext";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import PrivateRoute from "./Pages/Dashboard/privateRoutes";
 
 
 function App() {
 
-  // const [token, setToken] = useState(null)
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const authToken = Cookies.get("token");
+    if (authToken) {
+      setToken(authToken);
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
     <>
-      {/* <UserContext.Provider value={{ token, isLoggedIn, setToken, setIsLoggedIn }}>
+      <UserContext.Provider
+        value={{ token, isLoggedIn, setToken, setIsLoggedIn }}
+      >
         <Routes>
-          <Route exact path="*" element={<DashboardRoutes />} />
-          <Route path="/admin" element={<Website />} />
-          <Route path="/login" element={<Login/>} />
+          <Route element={<PrivateRoute />}>
+            <Route exact path="*" element={<DashboardRoutes />} />
+          </Route> 
+          {/* <Route path="/admin" element={<Website />} /> */}
+          <Route path="/login" element={<Login />} />
         </Routes>
-      </UserContext.Provider> */}
-      <TeamSection />
+      </UserContext.Provider>
     </>
   );
 }
