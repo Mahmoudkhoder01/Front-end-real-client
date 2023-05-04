a62fea7032efde194701b79a0c779ff9eb005d91
Yasmine Hannouf <yasmina.hannouf.2018@gmail.com>
2023-05-04 12:07:16 +0300

    adding shapes

--- src/Pages/Dashboard/About/About.js
+++ src/Pages/Dashboard/About/About.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import classes from "./About.module.css";
+import topCircle from "../../../Assets/Images/topcircle.svg";
+import dots from "../../../Assets/Images/dots.svg";
+
+import centerCircle from "../../../Assets/Images/centerCircle1.svg";
+import centerLinear from "../../../Assets/Images/centerCirclelinear.svg";
+import bottomCircle from "../../../Assets/Images/bottomCircle.svg";
+import yellowRectangle from "../../../Assets/Images/yellowrectangle.svg"
 
 
 
@@ -21,26 +28,42 @@ function Dashboard() {
   }
 
   return (
-    <div className={classes.container}>
-     <div className={classes.vectorContainer}>
-        
-        <input type="file" className={classes.formControlFile} id="image" onChange={handleImageChange} />
-      </div>
-      
+    <div className={classes.Aboutcontainer}>
+
+      {/* <img src={dots} alt="shapes" className={classes.topDots} /> */}
+
+
       <form onSubmit={handleSubmit}>
-      
-      <h2>About The CEO </h2>
-      
-      
-        <div className={classes.formGroup}>
-          <label htmlFor="description"></label>
-          <textarea className={classes.formControl} id="description" rows="3" value={description} onChange={handleDescriptionChange}></textarea>
+        <div className={classes.vectorContainer}>
+
+          <img src={centerCircle} alt="shapes" className={classes.centerCircle} />
+
+          
+
+
+
+          <h2>About The CEO</h2>
+          <img src={yellowRectangle} alt='shapes' className={classes.yellowRectangle} />
+
+          <div className={classes.formGroup}>
+            <label htmlFor="image">Upload Image:</label>
+            <input type="file" className={classes.formControlFile} id="image" onChange={handleImageChange} />
+          </div>
+
+          <div className={classes.formGroup}>
+            <label htmlFor="description">Description:</label>
+            <textarea className={classes.formControl} id="description" rows="3" value={description} onChange={handleDescriptionChange}></textarea>
+          </div>
+
+          <button type="submit" className={classes.btnPrimary}>Save Changes</button>
         </div>
-        <button type="submit" className={classes.btnPrimary}>Save Changes</button>
+        <img src={topCircle} alt='shapes' className={classes.topCircle} />
+        <img src={bottomCircle} alt="shapes" className={classes.bottomCircle} />
       </form>
-      
+
     </div>
   );
 }
+
 
 export default Dashboard;
