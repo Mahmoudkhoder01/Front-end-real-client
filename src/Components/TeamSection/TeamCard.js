93eddc6cd5e6eecce72c7357755491974a2e84a8
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-05-04 10:32:06 +0300

    finishing team card layout without fetching

--- src/Components/TeamSection/TeamCard.js
+++ src/Components/TeamSection/TeamCard.js
@@ -7,12 +7,12 @@ import Typography from '@mui/material/Typography';
 
 export default function TeamCard() {
   return (
-    <Card sx={{ maxWidth: 300,width:'fit', height: '350px',display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: 2 }}>
+    <Card sx={{ maxWidth: 300,width:250, height: '320px',display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: 2 }}>
       <img
         className={styles.teamImage}
         src={teamImage}
         alt="team card"
-        style={{ height: '70%' }}
+        style={{ height: '70%',objectFit: 'cover' }}
       />
       <CardContent sx={{display: 'flex', flexDirection:'column', alignItems:'center'}}>
         <Typography  variant="h5" style={{fontWeight:'600',marginBottom:'10px'}}>
