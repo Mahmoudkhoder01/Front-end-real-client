e5d11584b09c0cbc07ef101c1a2f8357f238955c
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-05-04 11:55:56 +0300

    finishing fetch

--- src/Components/TeamSection/TeamCard.js
+++ src/Components/TeamSection/TeamCard.js
@@ -1,25 +1,19 @@
-import React from "react";
+import React from 'react';
 import styles from './TeamCard.module.css';
-import teamImage from '../../Assets/Images/download.jpeg';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-export default function TeamCard() {
+export default function TeamCard({ name, description, image }) {
   return (
-    <Card sx={{ maxWidth: 300,width:250, height: '320px',display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: 2 }}>
-      <img
-        className={styles.teamImage}
-        src={teamImage}
-        alt="team card"
-        style={{ height: '70%',objectFit: 'cover' }}
-      />
-      <CardContent sx={{display: 'flex', flexDirection:'column', alignItems:'center'}}>
-        <Typography  variant="h5" style={{fontWeight:'600',marginBottom:'10px'}}>
-          Lizard
+    <Card sx={{ maxWidth: 300, width: 250, height: '320px', display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: 2 }}>
+      <img className={styles.teamImage} src={`http://localhost:5000/${image}`} alt="team card" style={{ height: '70%', objectFit: 'cover' }} />
+      <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography variant="h5" style={{ fontWeight: '600', marginBottom: '10px' }}>
+          {name}
         </Typography>
-        <Typography  color="text.secondary"  style={{fontWeight:'500', fontSize:'1rem'}}>
-          hi jaljflka;jlak;jaljflka       
+        <Typography color="text.secondary" style={{ fontWeight: '500', fontSize: '1.1rem' }}>
+          {description}
         </Typography>
       </CardContent>
     </Card>
