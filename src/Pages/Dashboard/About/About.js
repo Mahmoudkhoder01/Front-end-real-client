b9a6b9d07b9668c90dc8059f29a1aade02037763
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-05-05 10:57:54 +0300

    fix about

--- src/Pages/Dashboard/About/About.js
+++ src/Pages/Dashboard/About/About.js
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import classes from "./About.module.css";
-import bottomLeftCircle from "../../../Assets/Images/bottomLeftCircle.svg"
-import dots from "../../../Assets/Images/yellowDots.svg"
+import bottomLeftCircle from "../../../Assets/Images/bottomLeftCircle.svg";
+import dots from "../../../Assets/Images/yellowDots.svg";
 import centerCircle from "../../../Assets/Images/centerCircle1.svg";
 import bottomRightCircle from "../../../Assets/Images/centerCirclelinear.svg";
 
-
-
-function Dashboard() {
-  
-
+export default function About() {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Send the image and description to the server to update the dashboard
   };
 
-export default function About() {
   return (
     <div className={classes.container}>
       <img
         src={bottomLeftCircle}
         width="200px"
         className={classes.bottomLeftCircle}
+        alt=""
       />
-      <img src={dots} width="40px" className={classes.dots} />
-      <img src={centerCircle} className={classes.centerCircle} />
-      <img src={dots} width="40px" className={classes.topRightDots} />
+      <img src={dots} width="40px" className={classes.dots} alt="" />
+      <img src={centerCircle} className={classes.centerCircle} alt="" />
+      <img src={dots} width="40px" className={classes.topRightDots} alt="" />
       <img
         src={bottomRightCircle}
         width="200px"
         className={classes.bottomRightCircle}
+        alt=""
       />
       <div className={classes.uploadImage}>
         <div>
@@ -47,8 +44,6 @@ export default function About() {
           </label>
         </div>
       </div>
-
-      
     </div>
-  )
+  );
 }
