21a800538771cdc73af9b44a28e54bf370b9ea22
abdelrahman <abdelrahman.assoum@gmail.com>
2023-05-08 00:09:26 +0300

    draft projects

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from "./project.module.css"
-
 export default function ProjectCard(props) {
+  console.log(props)
   return (
-    <div class="card">
-         <img src={props.images} alt="project" />
-            <div class="info">
+    <div className={styles.card}>
+         <img src={props.image} alt="project" />
+            <div className={styles.info}>
                  <h3>{props.title}</h3>
                   <p>{props.description}</p>
             </div>
--- src/Pages/Viewer/Projects/Projects.js
+++ src/Pages/Viewer/Projects/Projects.js
@@ -1,9 +1,52 @@
-import classes from './Project.module.css'
+import classes from "./Project.module.css";
+import ProjectCard from "../../../Components/ProjectCard/ProjectCard";
+import imagetest1 from "../../../Assets/Images/Drafts/WhatsApp Image 2023-02-12 at 1.17.01 PM.jpeg"
+
+import pic1 from "../../../Assets/Images/portfolio1.jpg";
+import pic2 from "../../../Assets/Images/portfolio2.jpg";
+import pic3 from "../../../Assets/Images/portfolio3.jpg";
+// import reusabel fetch data
+// import useFetch from "../../../Components/useFetch/useFetch";
+// import Loading from "../../../Components/LoadingAnimation/Loading";
+
 
 export default function Projects() {
+
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className={classes.cards}>
+        <ProjectCard
+          title="Project Test"
+          image={pic1}
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+        />
+        <ProjectCard
+          title="Project Test"
+          image={pic2}
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+        />
+        <ProjectCard
+          title="Project Test"
+          image={pic3}
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+        />
+        <ProjectCard
+          title="Project Test"
+          image={pic1}
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+        />
+        <ProjectCard
+          title="Project Test"
+          image={pic2}
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+        />
+        <ProjectCard
+          title="Project Test"
+          image={pic3}
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+        />
+      </div>
+    </>
+  );
 }
